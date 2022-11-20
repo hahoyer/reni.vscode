341ac4023c8ddee29afecdaa03eae6a111bae35e
Harald Herbert Hoyer <hahoyer@gmail.com>
2022-11-20 14:36:33 +0100

    change

--- source/main.ts
+++ source/main.ts
@@ -1,81 +1,37 @@
-import * as vscode from 'vscode';
-import {func} from 'edge';
+'use strict';
 
-const tokenTypes = new Map<string, number>();
-const tokenModifiers = new Map<string, number>();
+import { workspace, Disposable, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind, InitializeParams } from 'vscode-languageclient';
+import { Trace } from 'vscode-jsonrpc';
 
-const legend = (function () {
-	const tokenTypesLegend = [
-		'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
-		'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
-		'method', 'macro', 'variable', 'parameter', 'property', 'label'
-	];
-	tokenTypesLegend.forEach((tokenType, index) => tokenTypes.set(tokenType, index));
+export function activate(context: ExtensionContext) {
 
-	const tokenModifiersLegend = [
-		'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
-		'modification', 'async'
-	];
-	tokenModifiersLegend.forEach((tokenModifier, index) => tokenModifiers.set(tokenModifier, index));
+    let reni = 'a:/develop/Reni/dev/out/Debug/ReniLSP.exe';
+    let reniServerOptions: ServerOptions = {
+        run: { command: reni },
+        debug: { command: reni }
+    }
 
-	return new vscode.SemanticTokensLegend(tokenTypesLegend, tokenModifiersLegend);
-})();
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: [
+            {
+                language: "reni",
+                scheme: "file"
+            }
+        ],
+        synchronize: {
+            configurationSection: 'reni.lang',
+            fileEvents: workspace.createFileSystemWatcher('**/*.reni')
+        },
+    }
 
-export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'reni'}, new DocumentSemanticTokensProvider(), legend));
-}
+    // Create the language client and start the client.
+    const client = new LanguageClient('reni.lang', 'reni language', reniServerOptions, clientOptions);
+    client.trace = Trace.Verbose;
+    let disposable = client.start();
 
-interface IParsedToken {
-	line: number;
-	startCharacter: number;
-	length: number;
-	tokenType: string;
-	tokenModifiers: string[];
-}
-
-class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
-	async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
-		const allTokens = this._parseText(document.getText());
-		const builder = new vscode.SemanticTokensBuilder();
-		allTokens.forEach((token) => {
-			builder.push(token.line, token.startCharacter, token.length, this._encodeTokenType(token.tokenType), this._encodeTokenModifiers(token.tokenModifiers));
-		});
-		return builder.build();
-	}
-
-	private _encodeTokenType(tokenType: string): number {
-		if (tokenTypes.has(tokenType)) {
-			return tokenTypes.get(tokenType)!;
-		} else if (tokenType === 'notInLegend') {
-			return tokenTypes.size + 2;
-		}
-		return 0;
-	}
-
-	private _encodeTokenModifiers(strTokenModifiers: string[]): number {
-		let result = 0;
-		for (let i = 0; i < strTokenModifiers.length; i++) {
-			const tokenModifier = strTokenModifiers[i];
-			if (tokenModifiers.has(tokenModifier)) {
-				result = result | (1 << tokenModifiers.get(tokenModifier)!);
-			} else if (tokenModifier === 'notInLegend') {
-				result = result | (1 << tokenModifiers.size + 2);
-			}
-		}
-		return result;
-	}
-
-	private _parseText(text: string): IParsedToken[] {
-		var csharpCall = func('async (text) => new[]{new{line=1,column= 1,length=2,tokenType="comment",tokenModifiers= new[]{"block"}}}');
-		var result = csharpCall(text);
-		return result;
-	}
-
-	private _parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
-		const parts = text.split('.');
-		return {
-			tokenType: parts[0],
-			tokenModifiers: parts.slice(1)
-		};
-	}
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
 }
--- typings-custom/edge.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "edge" {
-    export function func (code: string): (text: string) => any;
-  }
-  
